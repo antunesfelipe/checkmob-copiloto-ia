f85c5e12fe05e6840d98ae349ec73af388af03aa
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-04-24 02:31:06 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
-
-port = int(os.environ.get("PORT", 10000))  # <-- isso aqui é essencial na Render
+import health_app
 
 if __name__ == "__main__":
-    uvicorn.run("health_app:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(health_app.app, host="0.0.0.0", port=port)
