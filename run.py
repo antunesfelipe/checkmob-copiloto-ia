1e77e526f6ccb8715946c423012a362ac99e8391
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-14 22:39:41 -0300

    Update run.py
    ew

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
-import health_app
+from health_app import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(health_app.app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
