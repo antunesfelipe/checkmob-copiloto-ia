de1b310a02bc26c45a7fbdf1a2efd04c78b7deb0
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-14 22:39:23 -0300

    Update run.py
    aad

--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,7 @@
 import os
+import uvicorn
 from health_app import app
-from threading import Thread
 
 if __name__ == "__main__":
-    # Só para manter compatibilidade com Render (mesmo se sem HTTP)
     port = int(os.environ.get("PORT", 10000))
-    
-    # Rodando FastAPI para manter porta aberta e evitar erro de "No open ports"
-    import uvicorn
-    Thread(target=lambda: uvicorn.run(app, host="0.0.0.0", port=port)).start()
+    uvicorn.run(app, host="0.0.0.0", port=port)
