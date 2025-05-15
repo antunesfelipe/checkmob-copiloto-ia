347988d01376edb603fc974997660f0de6a7e033
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-15 02:49:53 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,7 @@
 import os
 from health_app import app
-from threading import Thread
+import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    import uvicorn
-    Thread(target=lambda: uvicorn.run(app, host="0.0.0.0", port=port)).start()
+    uvicorn.run(app, host="0.0.0.0", port=port)
