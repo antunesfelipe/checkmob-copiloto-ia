d8b361ce30c169a3faf65f2263b9c74144541b3d
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-15 04:04:41 -0300

    Update health_app.py

--- health_app.py
+++ health_app.py
@@ -1,7 +1,16 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"mensagem": "Copiloto Checkmob IA rodando com sucesso 🚀"}
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.api_route("/", methods=["GET", "HEAD"])
 def read_root():
     return {"mensagem": "Copiloto Checkmob IA rodando com sucesso 🚀"}
+
