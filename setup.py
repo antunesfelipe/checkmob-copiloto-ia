4b30f1633810c81fd3f4f1df77d9968475dc045c
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-15 00:03:56 -0300

    Update setup.py
    aaa

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="checkmob_copiloto",
     version="0.1",
-    packages=find_packages(where="backend", include=["onyx*", "ee*", "checkmob_copiloto*"]),
+    packages=find_packages(where="backend", include=[
+        "checkmob_copiloto*", "onyx*", "ee*", "onyx.redis*"
+    ]),
     package_dir={"": "backend"},
     include_package_data=True,
 )
