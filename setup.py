408674d4f55ecf29db5396766a1cb679adffb099
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-15 00:33:28 -0300

    Update setup.py
    ddd

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name="checkmob_copiloto",
     version="0.1",
     packages=find_packages(where="backend", include=[
-        "checkmob_copiloto*", "onyx*", "ee*", "onyx.redis*"
+        "checkmob_copiloto*", "onyx*", "ee*", "onyx.redis*", "shared_configs*"
     ]),
     package_dir={"": "backend"},
     include_package_data=True,
 )
+
