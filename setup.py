095f3d2f340e2f3e61eb5ae65ed1d7b01a10968e
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-15 04:01:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name="checkmob_copiloto",
     version="0.1",
-    packages=find_packages(where="backend", include=[
-        "checkmob_copiloto*", "onyx*", "ee*", "onyx.redis*", "shared_configs*"
+    packages=find_packages(include=[
+        "checkmob_copiloto*",
+        "onyx*",
+        "ee*",
+        "onyx.redis*",
+        "shared_configs*",
+        "model_server*",
+        "alembic*",
+        "alembic_tenants*",
+        "scripts*",
+        "tests*",             # se for importar testes ou rodar via setup
     ]),
-    package_dir={"": "backend"},
+    package_dir={"": "."},
     include_package_data=True,
 )
-
