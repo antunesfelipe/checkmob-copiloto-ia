0c665dd5b6386c1603a2d46804b140ead7d3c78a
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-21 17:45:35 -0300

    Update health_app.py

--- health_app.py
+++ health_app.py
@@ -1,8 +1,21 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# @app.head("/")
+# def read_root():
+#     return {"mensagem": "Copiloto Checkmob IA rodando com sucesso 🚀"}
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-@app.head("/")
-def read_root():
-    return {"mensagem": "Copiloto Checkmob IA rodando com sucesso 🚀"}
+class Pergunta(BaseModel):
+    pergunta: str
+
+@app.post("/pergunta")
+async def responder(pergunta: Pergunta):
+    return {"resposta": "Ainda estou aprendendo, mas recebi sua pergunta!"}
+
