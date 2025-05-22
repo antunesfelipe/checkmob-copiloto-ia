73ead67817bb2e0894805bef5140a5d1183369d3
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-22 17:55:09 -0300

    Update reader_indexer.py

--- reader_indexer.py
+++ reader_indexer.py
@@ -1,12 +1,10 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding  # ✅ Caminho correto
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 import os
 
 def carregar_ou_criar_indice(pasta_docs="docs", pasta_indice="index_storage"):
-    # Define modelo local de embedding
-    # embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/paraphrase-MiniLM-L3-v2")
-
+    # Modelo leve e eficiente, compatível com Render Free (512MiB)
+    embed_model = HuggingFaceEmbedding(model_name="intfloat/e5-small-v2")
 
     if os.path.exists(pasta_indice):
         storage = StorageContext.from_defaults(persist_dir=pasta_indice)
