6877c214b5a2bd17b431587ffbacdfdb8d85df5a
antunesfelipe <56726143+antunesfelipe@users.noreply.github.com>
2025-05-22 17:42:06 -0300

    Update reader_indexer.py

--- reader_indexer.py
+++ reader_indexer.py
@@ -4,7 +4,9 @@ import os
 
 def carregar_ou_criar_indice(pasta_docs="docs", pasta_indice="index_storage"):
     # Define modelo local de embedding
-    embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    # embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/paraphrase-MiniLM-L3-v2")
+
 
     if os.path.exists(pasta_indice):
         storage = StorageContext.from_defaults(persist_dir=pasta_indice)
